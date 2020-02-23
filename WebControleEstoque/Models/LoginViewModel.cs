3ed9f113e7fdde0163cad6ee99c5f27c0dde33ca
igorcavichiolle <igorgabrielcavichiolle@hotmail.com>
2020-02-23 10:48:08 -0300

    Upload Project

---
+++ WebControleEstoque/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebControleEstoque.Models
+{
+    public class LoginViewModel
+    {
+        [Display(Name="Usuario:")]
+        public string Usuario { get; set; }
+        [Display(Name = "Senha:")]
+        public string Senha { get; set; }
+        [Display(Name = "Lembrar Me")]
+        public bool LembrarMe { get; set; }
+    }
+}
